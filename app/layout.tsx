98cbbdd4a7137bca5ca6b10de5b11dd63902cfb2
Frank Simon <f.m.sim@t-online.de>
2024-06-16 14:21:30 +0200

    modified layout.tsx and page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        This is being rendered in the layout{children}
+      </body>
     </html>
   );
 }
